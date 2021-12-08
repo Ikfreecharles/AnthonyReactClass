486e1f2bfde3a4a92ad18c45701754a501396a90
ikfreecharles <sleektrends7@gmail.com>
2021-12-08 23:38:39 +0100

    input exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+   constructor(prop) {
+      super();
+      this.state = {
+         input: "",
+      };
+   }
+   render() {
+      return (
+         <div className="App">
+            <h1>Welcome to react</h1>
+            {console.log(this.state.input)}
+            <input
+               type="text"
+               name="name"
+               id="name"
+               value={this.state.input}
+               onChange={(e) => this.setState({ input: e.target.value })}
+            />
+         </div>
+      );
+   }
 }
 
 export default App;
